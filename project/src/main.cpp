e7f7a70fc352825c9dc95515c022a3cecd980a8c
Alexander <ighost99@gmail.com>
2018-11-11 14:07:35 +0300

    Main file was cleared

--- project/src/main.cpp
+++ project/src/main.cpp
@@ -9,16 +9,6 @@ int main() {
     std::string path = "../data/D7.TXT";
     Matrix* A = new Matrix;
     A->readLinearSystemFromFile(path);
-    Matrix* A1 = createTridiagonalMatrix(1);
-    //cout << "A1 is" << endl;
-    //A1->matrixPrint();
-    Matrix* result = Jacobi(A);
-    //Matrix* result = fixedPointIterationSolve(A);
-    //Matrix* result = gaussLinearSolve(A);
-    //Matrix* result = SOR(A1);
-    //Matrix* result = Seidel(A1);
-    //Matrix* result = QRDecompositionSolve(A, nullptr, nullptr);
-    cout << "Result is" << endl;
-    result->matrixPrint();
+    delete A;
     return 0;
 }
