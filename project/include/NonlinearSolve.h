0dd2a226bb6ff137fb755b86291d6b929d225b02
Alexander <ighost99@gmail.com>
2019-01-08 20:50:07 +0300

    Tests 1 - 3 were added

--- project/include/NonlinearSolve.h
+++ project/include/NonlinearSolve.h
@@ -6,10 +6,19 @@
 #define LAB_1_NONLINEARSOLVE_H
 
 #include <iostream>
+#include <cmath>
+#include "../include/Interpolation.h"
 
 using std::cout;
 using std::endl;
 
-void test();
+// TODO: finish localization of roots
+//double* rootsLocale(double* _mesh);
+
+// TODO: write rest tests
+// Functions
+double f1(double x);
+double f2(double x);
+double f3(double x);
 
 #endif //LAB_1_NONLINEARSOLVE_H
--- project/src/NonlinearSolve.cpp
+++ project/src/NonlinearSolve.cpp
@@ -4,6 +4,16 @@
 
 #include "../include/NonlinearSolve.h"
 
-void test() {
-    cout << "Hello, world" << endl;
+double f1(double x) {
+    return (x - 0.1) * (x - 0.22) * (x - 0.55) * (x - 0.7) * (x - 0.75);
+}
+
+double f2(double x) {
+    return sqrt(x + 1) - 1;
+}
+
+double f3(double x) {
+    double x2 = x * x;
+    double x3 = x2 * x;
+    return 35 * x3 - 67 * x2 - 3 * x + 3;
 }
