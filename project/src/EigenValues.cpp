56b7bfe7b67ebdc1f7831dcc3f5769710ef9e733
Alexander <ighost99@gmail.com>
2019-01-18 18:48:34 +0300

    Reverse iterations method was added. Reyleigh method was added.

--- project/src/EigenValues.cpp
+++ project/src/EigenValues.cpp
@@ -93,3 +93,184 @@ void shiftMatrix(Matrix* _A, double _shift) {
         _A->matrixGet()[i][i] += _shift;
     }
 }
+
+Matrix* Reverse(Matrix* _A, double eigen) {
+    double eps = 1e-3;
+    int rows = _A->rowsGet();
+    Matrix* res = new Matrix;
+    Matrix* Q = new Matrix;
+    Matrix* R = new Matrix;
+    Matrix* M = new Matrix;
+    res->matrixNullSet(rows,1);
+    M->matrixNullSet(rows, rows);
+    for (int i = 0; i < rows; i++) {
+    	res->matrixGet()[i][0] = 1.0;
+    }
+    double div = norm(res);
+    for (int i = 0; i < rows; i++) {
+        res->matrixGet()[i][0] /= div;
+    }
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < rows; j++) {
+            M->matrixGet()[i][j] = _A->matrixGet()[i][j];
+            if (i == j) {
+                M->matrixGet()[i][j] -= eigen;
+            }
+        }
+    }
+    QRDecompositionSolve(M,Q,R);
+    int n = 0;
+    Matrix* prev = new Matrix;
+    do {
+        n++;
+        prev = Matrix::getCopy(res);
+        Matrix* t = QRBackTurn(Q,R,res);
+        res = Matrix::getCopy(t);
+        Q->matrixTranspose();
+        double div = norm(res);
+        for (int i = 0; i < rows; i++) {
+            res->matrixGet()[i][0] /= div;
+        }
+    } while (1.0 - fabs(cosinus(res, prev)) >= eps);
+    delete prev;
+    cout << "Number of iterations is " << n << endl;
+    return res;
+}
+
+double Rayleigh(Matrix* _A, Matrix* res) {
+    double eps = 1e-14;
+    double eigen = 0;
+    int rows = _A->rowsGet();
+    Matrix* temp = new Matrix;
+    Matrix* M1 = new Matrix;
+    Matrix* M = new Matrix;
+    M->matrixNullSet(rows,rows + 1);
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < rows; j++) {
+            M->matrixGet()[i][j] = _A->matrixGet()[i][j];
+        }
+    }
+    temp->matrixNullSet(rows, rows + 1);
+    int n = 0;
+    double div = norm(res);
+    for (int i = 0; i < rows; i++) {
+        res->matrixGet()[i][0] /= div;
+    }
+
+    double prevEigen = 0.0;
+
+    do {
+        n++;
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < rows; ++j) {
+                M->matrixGet()[i][j] = _A->matrixGet()[i][j];
+            }
+        }
+        prevEigen = eigen;
+        eigen = 0;
+        M1 = Matrix::matrixComp(_A, res);
+        for (int i = 0; i < rows; i++) {
+            eigen += M1->matrixGet()[i][0] * res->matrixGet()[i][0];
+        }
+        for (int i = 0; i < rows; i++) {
+            M->matrixGet()[i][i] -= eigen;
+        }
+
+        for (int i = 0; i < rows; i++) {
+            M->matrixGet()[i][rows] = res->matrixGet()[i][0];
+        }
+        temp = Matrix::getCopy(M);
+        Matrix* Q = new Matrix;
+        Matrix* R = new Matrix;
+        QRDecompositionSolve(temp, Q, R);
+        res = QRBackTurn(Q, R, res);
+        double div = norm(res);
+        for (int i = 0; i < rows; i++) {
+            res->matrixGet()[i][0] /= div;
+        }
+    } while (fabs(prevEigen - eigen) > eps);
+    M1 = Matrix::matrixComp(_A, res);
+    double div1 = norm(M1);
+    for (int i = 0; i < rows; i++) {
+        M1->matrixGet()[i][0] /= div1;
+    }
+    cout << "Eigenvector for eigenvalue = " << eigen << " is (" << n << " iterations)" << endl;
+    M1->matrixPrint();
+    return eigen;
+}
+
+double cosinus(Matrix* vec1, Matrix* vec2) {
+    if ((vec1->rowsGet() != vec2->rowsGet()) || (vec1->colsGet() != 1) || (vec2->colsGet() != 1)) {
+        cout << "Vectors are incorrect" << endl;
+        return -1.0;
+    }
+    double res = 1.0;
+    res *= scalarProd(vec1, vec2);
+    res /= norm(vec1);
+    res /= norm(vec2);
+    return res;
+}
+
+double scalarProd(Matrix* vec1, Matrix* vec2) {
+    if ((vec1->rowsGet() != vec2->rowsGet()) || (vec1->colsGet() != 1) || (vec2->colsGet() != 1)) {
+        cout << "Vectors are incorrect" << endl;
+        return -1.0;
+    }
+    int rows = vec1->rowsGet();
+    double sum = 0.0;
+    for (int i = 0; i < rows; ++i) {
+        sum += vec1->matrixGet()[i][0] * vec2->matrixGet()[i][0];
+    }
+    return sum;
+}
+
+bool eigenvalueCheck(Matrix* _A, Matrix* _eigenVals) {
+    if (_A->rowsGet() != _A->colsGet()) {  // Exception
+        cout << "Matrix A is incorrect" << endl;
+        return false;
+    }
+    int matrixDimension = _A->rowsGet();
+    int nOfVals = _eigenVals->rowsGet();
+    auto* E = new Matrix;
+    E->matrixOneSet(matrixDimension, matrixDimension);
+    for (int i = 0; i < nOfVals; ++i) {
+        Matrix* B = Matrix::matrixDiff(_A, Matrix::matrixConstComp(E, _eigenVals->matrixGet()[i][0]));
+        if (gaussLinearSolve(B)) {
+            cout << "Eigenvalue = " << _eigenVals->matrixGet()[i][0] << " is NOT CORRECT" << endl;
+        } else {
+            cout << "Eigenvalue = " << _eigenVals->matrixGet()[i][0] << " is CORRECT" << endl;
+        }
+        delete B;
+    }
+    delete E;
+    return true;
+}
+
+void eigenvectorCheck(Matrix* _A, Matrix* _eigenVector, double _eigenValue) {
+    if ((_A->rowsGet() != _A->colsGet()) || (_A->rowsGet() != _eigenVector->rowsGet())) {  // Exception
+        cout << "Matrix A or eigenvector are incorrect" << endl;
+        return;
+    }
+    int matrixDimension = _A->rowsGet();
+    Matrix* b = Matrix::matrixConstComp(_eigenVector, _eigenValue);
+    Matrix* B = new Matrix;
+    B->matrixNullSet(matrixDimension, matrixDimension + 1);
+    for (int i = 0; i < matrixDimension; ++i) {
+        for (int j = 0; j < matrixDimension + 1; ++j) {
+            if (j == matrixDimension) {
+                B->matrixGet()[i][j] = b->matrixGet()[i][0];
+            } else {
+                B->matrixGet()[i][j] = _A->matrixGet()[i][j];
+            }
+        }
+    }
+    Matrix* checkSolution = gaussLinearSolve(B);
+    Matrix* error = Matrix::matrixDiff(checkSolution, _eigenVector);
+    cout << "Error vector for eigenvalue = " << _eigenValue << " is" << endl;
+    error->matrixPrint();
+    delete b;
+    delete B;
+    delete checkSolution;
+    delete error;
+}
+
--- project/src/main.cpp
+++ project/src/main.cpp
@@ -10,16 +10,54 @@ using std::cout;
 using std::endl;
 
 int main() {
+    // Matrix
     std::string path = "../data/EigenTest.txt";
     Matrix* A = new Matrix;
     A->readMatrixFromFile(path);
     cout << "Matrix A is " << endl;
     A->matrixPrint();
     Matrix* HesA = HessenbergForm(A);
-    cout << "Hessenberg is" << endl;
+    cout << "Hessenberg form of A is" << endl;
     HesA->matrixPrint();
-    Matrix* Result = QRDecompositionEigen(HesA);
-    cout << "Eigen values are " << endl;
-    Result->matrixPrint();
+    cout << endl;
+    // QR method
+    Matrix* Vals = QRDecompositionEigen(HesA);
+    cout << "Eigenvalues are (QR method)" << endl;
+    Vals->matrixPrint();
+    cout << endl;
+    // Eigenvalues check
+    eigenvalueCheck(HesA, Vals);
+    cout << endl;
+    // Reverse method
+    cout << "Eigenvectors are (Reverse method)" << endl;
+    for (int i = 0; i < Vals->rowsGet(); ++i) {
+        double eigenvalue = Vals->matrixGet()[i][0];
+        Matrix* r = Reverse(A, eigenvalue);
+        cout << "Eigenvector for eigenvalue = " << eigenvalue << " is" << endl;
+        r->matrixPrint();
+        cout << "Check..." << endl;
+        eigenvectorCheck(A, r, eigenvalue);
+        delete r;
+    }
+    cout << endl;
+    // Start vectors
+    string path1 = "../data/v1.txt";
+    string path2 = "../data/v2.txt";
+    string path3 = "../data/v3.txt";
+    string path4 = "../data/v4.txt";
+    Matrix* v1 = new Matrix;
+    Matrix* v2 = new Matrix;
+    Matrix* v3 = new Matrix;
+    Matrix* v4 = new Matrix;
+    v1->readMatrixFromFile(path1);
+    v2->readMatrixFromFile(path2);
+    v3->readMatrixFromFile(path3);
+    v4->readMatrixFromFile(path4);
+    // Rayleigh
+    cout << "Rayleigh method" << endl;
+    Rayleigh(A, v1);
+    Rayleigh(A, v2);
+    Rayleigh(A, v3);
+    Rayleigh(A, v4);
     return 0;
 }
